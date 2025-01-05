1bf168e8a533b531602d021281a936e55fead30f
Adalberto da Silva Junior <sbetiin_@hotmail.com>
2025-01-05 13:41:43 -0300

    Inicialização do repositório SMS Sender API

---
+++ src/main/java/dev/dadal/smssender/SmsSenderApiApplication.java
@@ -0,0 +1,13 @@
+package dev.dadal.smssender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsSenderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsSenderApiApplication.class, args);
+	}
+
+}
